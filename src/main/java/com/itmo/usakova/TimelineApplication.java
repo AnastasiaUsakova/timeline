99899dfb519aa01add2eaac79a2d0c53478f25e9
Your Name <you@example.com>
2020-02-25 23:18:30 +0300

    final preparation

--- src/main/java/com/itmo/usakova/TimelineApplication.java
+++ src/main/java/com/itmo/usakova/TimelineApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimelineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TimelineApplication.class, args);
 	}
-
 }
